30e66b6147fc7d42c78765502d66584fca5a8020
Andrzej Ressel <github@andrzejressel.pl>
2024-02-18 01:55:05 +0100

    Update common gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,49 +1,43 @@
-import pl.andrzejressel.deeplambdaserialization.buildplugin.License
+import pl.andrzejressel.plugin.License
 
 plugins {
-    id("java")
-    id("parent-plugin")
-    id("child-plugin")
+  id("java")
+  id("parent-plugin")
+  id("child-plugin")
 }
 
 group = "pl.andrzejressel"
 
-childPlugin { license = License.LGPL }
-
-repositories {
-    mavenCentral()
+childPlugin {
+  license = License.LGPL
+  disableJavaFormatter = true
+  childAndParentInTheSameProject = true
 }
+
+repositories { mavenCentral() }
 
 java { toolchain { languageVersion = JavaLanguageVersion.of(21) } }
 
 dependencies {
-    implementation("com.pulumi:pulumi:0.9.9")
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+  implementation("com.pulumi:pulumi:0.9.9")
+  testImplementation(platform("org.junit:junit-bom:5.9.1"))
+  testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
-tasks.test {
-    useJUnitPlatform()
-}
+tasks.test { useJUnitPlatform() }
 
-tasks.withType<JavaCompile>().configureEach {
-    options.compilerArgs.add("--enable-preview")
-}
+tasks.withType<JavaCompile>().configureEach { options.compilerArgs.add("--enable-preview") }
 
-tasks.withType<Test>().configureEach {
-    jvmArgs("--enable-preview")
-}
+tasks.withType<Test>().configureEach { jvmArgs("--enable-preview") }
 
-tasks.withType<JavaExec>().configureEach {
-    jvmArgs("--enable-preview")
-}
+tasks.withType<JavaExec>().configureEach { jvmArgs("--enable-preview") }
 
 tasks.jacocoTestReport {
-    dependsOn("test")
+  dependsOn("test")
 
-    executionData.setFrom(fileTree(layout.buildDirectory).include("/jacoco/*.exec"))
-    reports {
-        xml.required = true
-        html.required = true
-    }
+  executionData.setFrom(fileTree(layout.buildDirectory).include("/jacoco/*.exec"))
+  reports {
+    xml.required = true
+    html.required = true
+  }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,14 +1,12 @@
-plugins {
-    id("com.gradle.enterprise") version ("3.16.2")
-}
+plugins { id("com.gradle.enterprise") version ("3.16.2") }
 
 if (!System.getenv("CI").isNullOrEmpty()) {
-    gradleEnterprise {
-        buildScan {
-            termsOfServiceUrl = "https://gradle.com/terms-of-service"
-            termsOfServiceAgree = "yes"
-        }
+  gradleEnterprise {
+    buildScan {
+      termsOfServiceUrl = "https://gradle.com/terms-of-service"
+      termsOfServiceAgree = "yes"
     }
+  }
 }
 
 rootProject.name = "pulumi-java-string-interpolator"
